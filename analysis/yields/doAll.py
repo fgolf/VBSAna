c4940337f9ee220498224f25305e3cba13b11d40
fgolf <frank.golf@gmail.com>
2020-07-14 13:00:23 -0700

    add debug flags

--- analysis/yields/doAll.py
+++ analysis/yields/doAll.py
@@ -58,9 +58,9 @@ options = {
     #2016: "  doSS Data2016 new2016FRBins ReadBDT quiet ",
     #2017: "  doSS Data2017 ReadBDT quiet ",
     #2018: "  doSS Data2018 ReadBDT quiet ",
-    2016: "  doSS Data2016 new2016FRBins quiet",
-    2017: "  doSS Data2017 quiet",
-    2018: "  doSS Data2018 quiet",
+    2016: "  doSS Data2016 new2016FRBins quiet debug",
+    2017: "  doSS Data2017 quiet debug",
+    2018: "  doSS Data2018 quiet debug",
     }
 
 def make_objs(fpatts=[],options="",treename="t"):
